5d75e961e0d1ccddb85f3b605d4b4afb16287228
Jakub Poczatek <kubapoczatek2001@gmail.com>
2023-05-10 20:01:57 +0100

    Google Auth
    Sign in with google implemented.

--- app/src/main/java/org/wit/mtgcompanionv2/ui/auth/Login.kt
+++ app/src/main/java/org/wit/mtgcompanionv2/ui/auth/Login.kt
@@ -4,9 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import org.wit.mtgcompanionv2.R
 import org.wit.mtgcompanionv2.ui.home.Home
 import org.wit.mtgcompanionv2.databinding.LoginBinding
@@ -16,6 +22,7 @@ class Login : AppCompatActivity() {
 
     private lateinit var loginRegisterViewModel : LoginRegisterViewModel
     private lateinit var loginBinding : LoginBinding
+    private lateinit var startForResult : ActivityResultLauncher<Intent>
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +37,9 @@ class Login : AppCompatActivity() {
             createAccount(loginBinding.fieldEmail.text.toString(),
                 loginBinding.fieldPassword.text.toString())
         }
+        loginBinding.googleSignInButton.setSize(SignInButton.SIZE_WIDE)
+        loginBinding.googleSignInButton.setColorScheme(0)
+        loginBinding.googleSignInButton.setOnClickListener { googleSignIn() }
     }
 
     public override fun onStart() {
@@ -42,6 +52,8 @@ class Login : AppCompatActivity() {
 
         loginRegisterViewModel.firebaseAuthManager.errorStatus.observe(this, Observer
         { status -> checkStatus(status) })
+
+        setupGoogleSignInCallback()
     }
 
     //Required to exit app from Login Screen - must investigate this further
@@ -91,5 +103,32 @@ class Login : AppCompatActivity() {
             loginBinding.fieldPassword.error = null
         }
         return valid
+    }
+
+    private fun googleSignIn(){
+        val signInIntent = loginRegisterViewModel.firebaseAuthManager.googleSignInClient.value!!.signInIntent
+        startForResult.launch(signInIntent)
+    }
+
+    private fun setupGoogleSignInCallback(){
+        startForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                when(result.resultCode){
+                    RESULT_OK -> {
+                        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                        try {
+                            val account = task.getResult(ApiException::class.java)
+                            loginRegisterViewModel.authWithGoogle(account!!)
+                        } catch (e: ApiException) {
+                            Timber.i("Google sign in failed $e")
+                            Snackbar.make(loginBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                        }
+                        Timber.i("MTG Companion google result ${result.data}")
+                    }
+                    RESULT_CANCELED -> {
+
+                    } else -> { }
+                }
+            }
     }
 }
